0d254aa4344ac740a6dfaa666a3ed65b9ff846db
remimoul <neptunes92390@outlook.fr>
2023-01-24 22:39:36 +0100

    footer bootstrap test

--- index.php
+++ index.php
@@ -9,8 +9,18 @@
     <link
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 </head>
-<body>
+<body class="d-flex flex-column min-vh-100">
 <?php include_once('header.php'); ?>
 
+
+<?php
+echo "test";
+?>
+
+
+
+
 </body>
+
+<?php include_once('footer.php'); ?>
 </html>
